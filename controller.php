399415012d570d581b5a04a88252a942fbcbd53f
Katz Ueno <katzueno@deerstudio.com>
2016-04-28 16:30:10 +0000

    Initial commit v.0.1.0

---
+++ controller.php
@@ -0,0 +1,45 @@
+<?php
+namespace Concrete\Package\RescanFilesJob;
+
+use Concrete\Core\Package\Package;
+use Core;
+use Job;
+
+class Controller extends Package
+{
+    protected $pkgHandle = "rescan_files_job";
+    protected $appVersionRequired = '5.7.4.2';
+    protected $pkgVersion = '0.1.0';
+    protected $pkgAutoloaderMapCoreExtensions = true;
+    
+    public function getPackageDescription()
+    {
+        return t('Rescan entire files in the file manager');
+    }
+
+    public function getPackageName()
+    {
+        return t("Rescan All Files");
+    }
+
+    public function install()
+    {
+        $pkg = parent::install();
+        $this->installJobs($pkg);
+    }
+    public function upgrade()
+    {
+        $pkg = parent::upgrade();
+        $this->installJobs($pkg);
+    }
+    
+    protected function installJobs($pkg)
+    {
+        $jobHandle = 'rescan_all_files';
+        $job = Job::getByHandle($jobHandle);
+        if (!is_object($job)) {
+            Job::installByPackage($jobHandle, $pkg);
+        }
+        
+    }
+}
---
+++ jobs/rescan_all_files.php
@@ -0,0 +1,62 @@
+<?php
+namespace Concrete\Package\RescanFilesJob\Job;
+
+use QueueableJob;
+use \ZendQueue\Queue as ZendQueue;
+use \ZendQueue\Message as ZendQueueMessage;
+use File;
+use FileList;
+
+class RescanAllFiles extends QueueableJob
+{
+	public $jSupportsQueue = true;
+	
+	protected $files;
+
+    public function getJobName()
+    {
+        return t("Rescan All Files");
+    }
+
+    public function getJobDescription()
+    {
+        return t("Rescan all files in the file manager and regenerate thumbnails.");
+    }
+
+    public function start(ZendQueue $q)
+    {
+    	$list = new \Concrete\Core\File\FileList();
+    	$this->files = $list->getResults();
+
+    	foreach ($this->files as $f)
+    	{
+    		$fID = $f->getFileID();
+    		$q->send($fID);
+    	}
+    }
+
+    public function processQueueItem(ZendQueueMessage $msg)
+    {
+    	try
+    	{
+    		$f = File::getbyID($msg->body);
+    		if (is_object($f))
+    		{
+	        	$fv = $f->getApprovedVersion();
+	        	if (is_object($fv))
+	        	{
+	        		$fv->refreshAttributes();
+	        	}
+    		}
+        } catch (\Concrete\Flysystem\FileNotFoundException $e)
+        {
+            $errorMessage .= t('File %s could not be found.', $fv->getFilename()) . '<br/>';
+        }
+    }
+
+
+    public function finish(ZendQueue $q)
+    {
+    	return t('All file processed');
+    }
+}
